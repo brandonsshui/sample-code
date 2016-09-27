b79adc6384d03bebe4d5f0ee14b5ab9d4cf921fa
Brandon Hui <brandonsshui@gmail.com>
2016-09-27 16:26:16 -0700

    some sample code from the Ink project under NDA

---
+++ Scripts/app/services/eventService.js
@@ -0,0 +1,16 @@
+﻿
+(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .factory('$eventService', eventServiceFactory);
+
+    eventServiceFactory.$inject = ['$baseService', '$sabio'];
+
+    function eventServiceFactory($baseService, $sabio) {
+        var aSabioServiceObject = sabio.services.events;
+        var newService = $baseService.merge(true, {}, aSabioServiceObject, $baseService);
+
+        return newService;
+    }
+})();
